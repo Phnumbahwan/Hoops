e109113dff0cb34396e5be248bdeaa0a032cf459
Free Wifi <quickerz59@gmail.com>
2023-07-22 21:44:44 +0800

    Updates from the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Card } from 'flowbite-react';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-      <Card
-      className="max-w-sm"
-      href="#"
-    >
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-        <p>
-          Noteworthy technology acquisitions 2021
-        </p>
-      </h5>
-      <p className="font-normal text-gray-700 dark:text-gray-400">
-        <p>
-          Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
-        </p>
-      </p>
-    </Card>
-      </div>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/Main.jsx
@@ -0,0 +1,12 @@
+const Main = () => {
+  return (
+    <div className="grid grid-cols-5 w-screen h-screen">
+      <div className="col-span-3 bg-white">dwadddddddddd</div>
+      <div className="col-span-2 bg-[#FF4A73]">
+        <Button>Setup Game</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
