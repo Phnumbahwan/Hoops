b6293c2ec1fbdd63160d27d2de40d76fd7bd7f25
Free Wifi <quickerz59@gmail.com>
2023-07-28 00:21:55 +0800

    Added author name in the landing page

--- src/components/HomeBanner/left.jsx
+++ src/components/HomeBanner/left.jsx
@@ -4,9 +4,9 @@ const Left = () => {
             <img
                 src='https://ucarecdn.com/2c759b4b-2398-4aef-9075-a0eed29320a6/fireballhome.png'
                 alt="Description of the image"
-                className="w-64 h-64 object-cover mt-4"
+                className="w-64 h-64 object-cover m-4"
             />
-            <div className="coiny-font text-primary text-5xl">HOOOOPS!</div>
+            <div className="coiny-font text-primary text-5xl">HOOOOPS BY GIL!</div>
         </div>
     )
 }
