295f3522cffa06bc9346a08f1fb1fc1fb7601422
Free Wifi <quickerz59@gmail.com>
2023-07-28 00:13:57 +0800

    Added screenshot function

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,14 +1,37 @@
 import Header from "../components/Game/header";
 import TeamScore from "../components/Game/teamScore";
 import Table from "../components/Game/table";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router";
+import { useScreenshot, createFileName } from "usescreenshot-react";
 
 const Game = () => {
     const navigate = useNavigate();
     const [teamA, setTeamA] = useState(JSON.parse(Cookies.get('teamA')));
     const [teamB, setTeamB] = useState(JSON.parse(Cookies.get('teamB')));
     const [end, setEnd] = useState(false);
+
+    const { image, takeScreenshot, isLoading, isError } = useScreenshot();
+    const ref = useRef()
+
+    useEffect(() => {
+        if (image) {
+            const link = document.createElement('a');
+            link.href = image;
+            link.download = 'game_stat.png';
+            link.click();
+        }
+    }, [image])
+
+    const getImage = () => {
+        if (!ref.current) {
+            return
+        }
+        takeScreenshot(ref.current, {
+            backgroundColor: '#fff',
+            logging: false,
+        }).catch(console.log);
+    }
 
     const handleEndGame = () => {
         setEnd(true);
@@ -27,18 +50,20 @@ const Game = () => {
     return (
         <div className="bg-white text-black h-screen px-10 pb-5">
             <Header />
-            <TeamScore teamA={teamA} teamB={teamB} end={end} />
-            <Table t={'teamA'} team={teamA} setTeam={setTeamA} />
-            <Table t={'teamB'} team={teamB} setTeam={setTeamB} />
+            <div ref={ref}>
+                <TeamScore teamA={teamA} teamB={teamB} end={end} />
+                <Table t={'teamA'} team={teamA} setTeam={setTeamA} />
+                <Table t={'teamB'} team={teamB} setTeam={setTeamB} />
+            </div>
             {
                 end ? (
-                    <div className="text-center mt-[30px]">
-                        <button className="text-white bg-primary font-bold w-[300px] py-3 object-cover border-2 border-white hover:border-primary hover:bg-white hover:text-primary focus:outline-none">SCREENSHOT</button>
+                    <div className="text-center mt-[50px]">
+                        <button onClick={getImage} className="text-white bg-primary font-bold w-[300px] py-3 object-cover border-2 border-white hover:border-primary hover:bg-white hover:text-primary focus:outline-none">SCREENSHOT</button>
                         <button onClick={() => handleContinue()} className="text-white bg-primary font-bold w-[300px] py-3 object-cover border-2 border-white hover:border-primary hover:bg-white hover:text-primary focus:outline-none">CONTINUE GAME</button>
                         <button onClick={() => handleSetup()} className="text-white bg-primary font-bold w-[300px] py-3 object-cover border-2 border-white hover:border-primary hover:bg-white hover:text-primary focus:outline-none">SETUP GAME</button>
                     </div>
                 ) : (
-                    <div className="text-center mt-[30px]">
+                    <div className="text-center mt-[50px]">
                         <button onClick={() => handleEndGame()} className="text-white bg-primary font-bold w-[300px] py-3 object-cover border-2 border-white hover:border-primary hover:bg-white hover:text-primary focus:outline-none">END GAME</button>
                     </div>
                 )
